145669248daa8653129585e9832cea0eaceaae8d
MarcoRob <marcko.rp@gmail.com>
2018-04-30 11:07:19 -0500

    adding chart for ingresos/egresos

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Line} from "react-chartjs-2";
 
 export default class Dashboard extends React.Component {
 
@@ -16,6 +17,53 @@ export default class Dashboard extends React.Component {
     }
 
     render() {
+        const data = {
+            labels: ['Junio 2017', "", "", 'Julio 2017', "", 'Agosto 2017'],
+            datasets: [
+              {
+                label: 'Egresos',
+                fill: false,
+                lineTension: 0.1,
+                backgroundColor: 'rgba(75,192,192,0.4)',
+                borderColor: 'rgba(75,192,192,1)',
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: 'rgba(75,192,192,1)',
+                pointBackgroundColor: '#fff',
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                pointHoverBorderColor: 'rgba(220,220,220,1)',
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: [0, 1, 0.5, 2, 1, 0]
+              },
+              {
+                label: 'Ingresos',
+                fill: false,
+                lineTension: 0.1,
+                backgroundColor: '#1e88e5',
+                borderColor: '#1e88e5 ',
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: '#1e88e5',
+                pointBackgroundColor: '#fff',
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                pointHoverBorderColor: '#1e88e5',
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: [0, -1, -0.5, -2, -0.5, 0]
+              }
+            ]
+          };
         return (
             <div>
                 <div className="row">
@@ -75,6 +123,9 @@ export default class Dashboard extends React.Component {
                             <div className="center grey-text">
                                 Ingresos / Egresos
                             </div>  
+                            <Line data={data}
+                                  options={{maintainAspectRatio: true}}
+                            />
                         </div>
                     </div>
                     <div className="col s5">
